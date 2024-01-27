618ac6185a6dc945c18cb664eb31fe5c7b05428c
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-27 16:35:41 -0600

    Update chess_bot.py

--- chess_bot.py
+++ chess_bot.py
@@ -12,16 +12,12 @@ class ChessNN(nn.Module):
         super(ChessNN, self).__init__()
         self.fc1 = nn.Linear(269, 269)  # Input layer
         self.fc2 = nn.Linear(269, 269)  # Hidden layer 1
-        self.fc3 = nn.Linear(269, 269)  # Hidden layer 2
-        self.fc4 = nn.Linear(269, 269)  # Hidden layer 3
-        self.fc5 = nn.Linear(269, 1)    # Output layer
+        self.fc3 = nn.Linear(269, 1)    # Output layer
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = torch.sigmoid(self.fc5(x))  # Sigmoid activation for output
+        x = torch.sigmoid(self.fc3(x))  # Sigmoid activation for output
         return x
 
 
