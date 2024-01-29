8506d39d064a6d01a527a83f51f508f33130597c
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-28 18:58:48 -0600

    Update chess_bot.py

--- chess_bot.py
+++ chess_bot.py
@@ -178,7 +178,7 @@ def calculate_material_score(board):
 
 
 # White is the neural network bot, and black is the random bot
-def play_random_game(model, max_turns=12):
+def play_random_game(model, max_turns=24):
     global MOVE
 
     board = chess.Board()
