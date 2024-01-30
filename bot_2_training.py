30e62071d9348d28d12a21a76a81f9a405a7b7b7
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-29 21:23:41 -0600

    Update bot_2_training.py

--- bot_2_training.py
+++ bot_2_training.py
@@ -96,6 +96,8 @@ for epoch in range(num_epochs):
             val_loss += criterion(outputs, labels).item()
     
     print(f"Epoch [{epoch+1}/{num_epochs}] - Validation Loss: {val_loss/len(val_loader)}")
+    with open("fitness_log.txt", 'a') as file:
+        file.write(print(f"Epoch [{epoch+1}] - Validation Loss: {val_loss/len(val_loader)}"))
 
 # Save the trained model
 torch.save(model.state_dict(), 'chess_cnn_model.pth')
