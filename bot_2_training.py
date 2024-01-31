3ad9edd5e7a4887726ac3be1471ea43ecfec9ed4
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-30 20:10:18 -0600

    Update bot_2_training.py

--- bot_2_training.py
+++ bot_2_training.py
@@ -47,9 +47,9 @@ class ChessCNN(nn.Module):
     def __init__(self):
         super(ChessCNN, self).__init__()
         # Define the CNN layers
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=2, padding=0)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=2, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, padding=0)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, padding=0)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         # Adjusted input size for the fully connected layer
         self.fc1 = nn.Linear(in_features=64, out_features=128)  # Corrected input size
